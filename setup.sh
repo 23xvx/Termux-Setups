d5856c5ae05eff8f9cebae68b419df798c7dafe0
23xvx <120558411+23xvx@users.noreply.github.com>
2023-11-22 23:37:23 +0800

    Delete some additional packages

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ apt upgrade -y &> /dev/null
 apt update && apt install x11-repo tur-repo root-repo -y &> /dev/null 
 echo ""
 echo ${G}"Installing packages ─=≡Σ((( つ•̀ω•́)つ "
-apt install git wget neofetch proot-distro which tsu -y &> /dev/null 
+apt install git wget neofetch proot-distro which -y &> /dev/null 
 mkdir -p $PREFIX/var/lib/proot-distro/dlcache
 mkdir -p $PREFIX/var/lib/proot-distro/installed-rootfs 
-apt install xwayland termux-x11-nightly pulseaudio -y &> /dev/null 
+apt install termux-x11-nightly pulseaudio -y &> /dev/null 
 apt-mark unhold openssl apt termux-tools bash &> /dev/null 
 apt update &> /dev/null 
 echo ""
@@ -29,13 +29,12 @@ termux-setup-storage
 echo ""
 echo ${G}"Installing my own need packages :)"
 apt update &> /dev/null 
-apt install tigervnc xorg-xhost dbus cpufetch virglrenderer-android -y &> /dev/null
+apt install tigervnc xorg-xhost dbus virglrenderer-android -y &> /dev/null
 echo ""
 echo ${G}"It's boring ヽ(・×・´)ゞ "
-apt install cmake make python-pip openssh make ffmpeg -y &> /dev/null
+apt install cmake make openssh make python-yt-dlp -y &> /dev/null
 echo 
 echo ${G}"Shall we play some game ( ^ω^)"
-pip install yt-dlp &> /dev/null
 echo "vncserver -listen tcp :1 && DISPLAY=:1 xhost + " >> $PREFIX/bin/startvnc 
 echo "vncserver -kill :1" >> $PREFIX/bin/stopvnc
 echo "
